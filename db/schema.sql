640e135387779a7277a432057f2ab6736d1e2779
jsncorn <jsncourn@gmail.com>
2022-02-03 16:04:19 -0800

    added department table

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,8 @@
 DROP DATABASE IF EXISTS employees_db;
 CREATE DATABASE employees_db;
 USE employees_db;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(40) NOT NULL,
+)
