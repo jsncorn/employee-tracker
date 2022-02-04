b4ccdb100a89d3d7519d79f7bbf8e23a5d0b69d0
jsncorn <jsncourn@gmail.com>
2022-02-03 17:17:15 -0800

    added prompts

--- server.js
+++ server.js
@@ -11,3 +11,34 @@ const connection = mysql.createConnection({
     password: 'password',
     database: ''
 });
+
+connection.connect((err) => {
+    if(err) {
+        console.log(err);
+        res.status(500);
+    }
+    console.log('Successfully connected');
+    runPrompt();
+})
+
+connection.query = util.promisify(connection.query);
+
+function runPrompt() {
+    inquirer
+    .prompt({
+        name: 'action',
+        type: 'list',
+        message: 'What do you want to do?',
+        choiches: [
+            'View employees',
+            'View employees by department',
+            'View employees by manager',
+            'Add employee',
+            'Add department',
+            'Add role',
+            'Remove employee',
+            'Update employee role',
+            'Update employee manager'
+        ]
+    })
+}
